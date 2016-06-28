1c0efa96c0fddab8c651b89576ed0e19ad8cee6e
Edward Cai <ecai@guidewire.com>
2016-06-27 17:36:41 -0700

    basic parsing class functions (assuming no errors)

--- src/main/java/tora/parser/Parser.java
+++ src/main/java/tora/parser/Parser.java
@@ -1,6 +1,6 @@
 package tora.parser;
 
-import tora.parser.tree.ClassNode;
+import tora.parser.tree.*;
 
 public class Parser
 {
@@ -19,22 +19,26 @@ public class Parser
     return _classNode != null;
   }
 
-  public void parse() {
+  public ClassNode parse() {
     nextToken();
     parseClassStatement();
+    return _classNode;
   }
 
   private void parseClassStatement()
   {
     if( match(TokenType.CLASS) != null )
     {
+      nextToken();
       Tokenizer.Token className = match( TokenType.IDENTIFIER );
       if( className != null )
       {
+        nextToken();
         _classNode = new ClassNode( className.getValue() );
         if( match( '{' ) != null )
         {
-          parseClassBody();
+          nextToken();
+          parseClassBody(className.getValue());
           Tokenizer.Token end = match( '}' );
           _classNode.setTokens( className, end );
         }
@@ -42,14 +46,130 @@ public class Parser
     }
   }
 
-  private void parseClassBody()
+  private void parseClassBody(String className)
   {
-    // TODO
-    // parse constructor
-    // parse function definitions
-    // parse properties
-    // remember the static modifiers
-    // include bodies
+    while(match('}') == null) {
+      System.out.println(_currentToken.getValue());
+      // TODO
+      if (matchClassKeyword("constructor") != null) {
+        _classNode.addChild(parseConstructor(className));
+      } else if (matchClassKeyword("get") != null || matchClassKeyword("set") != null) {
+        _classNode.addChild(parseProperty());
+      } else if (matchClassKeyword("static") != null || match(TokenType.IDENTIFIER) != null) {
+        _classNode.addChild(parseFunction(className));
+      }
+      // remember the static modifiers
+      // include bodies
+    }
+  }
+
+  private ConstructorNode parseConstructor(String className) {
+    Tokenizer.Token start = _currentToken; //starting token is "constructor"
+    nextToken();
+    String args = "";
+    FunctionBodyNode body = null;
+    if (match('(') != null) {
+      args = parseArgs();
+      body = parseFunctionBody();
+    }
+    Tokenizer.Token end = match('}');
+    nextToken();
+    ConstructorNode node = new ConstructorNode(className, args, start, end);
+    node.addChild(body);
+    return node;
+  }
+
+  private FunctionNode parseFunction(String className) {
+    boolean isStatic = false;
+    String args = "";
+    FunctionBodyNode body = null;
+    Tokenizer.Token start = _currentToken;
+    if (matchClassKeyword("static") != null) {
+      isStatic = true;
+      nextToken();
+    }
+    Tokenizer.Token functionName = match(TokenType.IDENTIFIER);
+    nextToken();
+    if (match('(') != null) {
+      args = parseArgs();
+      body = parseFunctionBody();
+    }
+    Tokenizer.Token end = match('}');
+    nextToken();
+    FunctionNode node = new FunctionNode(functionName.getValue(), className, args, isStatic);
+    node.addChild(body);
+    return node;
+  }
+
+  private PropertyNode parseProperty() {
+
+    return null;
+  }
+
+  private String parseArgs() {
+    nextToken(); // '('
+    StringBuilder val = new StringBuilder();
+    while (match(')') == null) {
+      if (match(TokenType.IDENTIFIER) != null) {
+        concatToken(val);
+        nextToken();
+      }
+      if (match(',') != null) {
+        concatToken(val);
+        nextToken();
+      }
+    }
+    nextToken();
+    return val.toString();
+  }
+  
+  private void concatToken (StringBuilder val) {
+    if (match(TokenType.NUMBER) != null) {
+      val.append(" ");
+    }
+    val.append(_currentToken.getValue());
+    if (match(TokenType.KEYWORD) != null) {
+      val.append(" ");
+    }
+  }
+
+  private FunctionBodyNode parseFunctionBody() {
+    StringBuilder val = new StringBuilder();
+    concatToken(val); // '{'
+    int curlyCount = 1;
+    while (curlyCount > 0) {
+      nextToken();
+      if (match('}') != null) curlyCount--;
+      if (match('{') != null) curlyCount++;
+      concatToken(val);
+    }
+    return new FunctionBodyNode(val.toString());
+  }
+
+  private Tokenizer.Token match( char c )
+  {
+    return match(TokenType.PUNCTUATION, String.valueOf(c));
+  }
+
+  private Tokenizer.Token matchKeyword(String val)
+  {
+    return match(TokenType.KEYWORD, val);
+  }
+
+  /*Matches conditional keywords such as "constructor", which are only keywords within a class*/
+  private Tokenizer.Token matchClassKeyword(String val)
+  {
+    return match(TokenType.IDENTIFIER, val);
+  }
+
+  private Tokenizer.Token match(TokenType type, String val) {
+    if (_currentToken.getType() == type &&
+            _currentToken.getValue().equals(val)) {
+      Tokenizer.Token t = _currentToken;
+      return t;
+    } else {
+      return null;
+    }
   }
 
   private Tokenizer.Token match( TokenType type )
@@ -57,23 +177,6 @@ public class Parser
     if( _currentToken.getType() == type )
     {
       Tokenizer.Token t = _currentToken;
-      nextToken();
-      return t;
-    }
-    else
-    {
-      return null;
-    }
-  }
-
-
-  private Tokenizer.Token match( char c )
-  {
-    if( _currentToken.getType() == TokenType.PUNCTUATION &&
-        _currentToken.getValue().charAt( 0 ) == c )
-    {
-      Tokenizer.Token t = _currentToken;
-      nextToken();
       return t;
     }
     else
--- src/test/java/tora/parser/ParserTest.java
+++ src/test/java/tora/parser/ParserTest.java
@@ -5,9 +5,22 @@ package tora.parser;
  */
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class ParserTest {
+    private static ScriptEngine engine;
+
+    @BeforeClass
+    public static void beforeClass() {
+        engine = new ScriptEngineManager().getEngineByName("nashorn");
+    }
+
+
     String Edward = "shit";
     @Test
     public void parseBasicTest() {
@@ -20,6 +33,22 @@ public class ParserTest {
         Parser parser = new Parser(tokenizer);
         parser.parse();
         Assert.assertEquals(Edward, "shit");
+    }
+
+    @Test
+    public void parseConstructor() throws ScriptException {
+        String testCode =
+                "class DemoClass { constructor(a, b, c) {this.foo = a + b + c}   " +
+                "static staticFoo() { return 42; } " +
+                "bar() { return this.foo; } " +
+                "}";
+        String genCode = new Parser(new Tokenizer(testCode)).parse().genCode();
+        System.out.println(genCode);
+        engine.eval(genCode);
+        engine.eval("var dem = new DemoClass(10,30,2);");
+        Assert.assertEquals(42.0, engine.eval("dem.foo"));
+        Assert.assertEquals(42.0, engine.eval("dem.bar()"));
+        Assert.assertEquals(42, engine.eval("DemoClass.staticFoo()"));
     }
 
 }
